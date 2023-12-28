39f5bcecc1b7f51fff190ef1a91798a80275eea8
Niklaus-Mk <niklausmikaelsonboss@gmail.com>
2023-12-27 21:14:08 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // Busca en todas las subcarpetas y achivos del directorio src los archivos con extension .jsx
+  content: ['index.html', './src/**/*.jsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
